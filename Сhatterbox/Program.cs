154a3d8855f948c4392e5569ffc8d233d19862f5
Влад Безедика <sttasea@mail.ru>
2023-03-27 23:46:06 +0300

    Exx

--- Сhatterbox/Program.cs
+++ Сhatterbox/Program.cs
@@ -4,13 +4,14 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Сhatterbox
 {
     class Program
     {
         static int localPort; // порт приема сообщений
-        static Socket listeningSocket; // Сокет
+        static Socket listenSocket; // Сокет
         static List<IPEndPoint> clients = new List<IPEndPoint>(); // Список "подключенных" клиентов
         static void Main(string[] args)
         {
@@ -20,10 +21,10 @@ namespace Сhatterbox
             Console.WriteLine();
             try
             {
-                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // Создание сокета
-                Task listeningTask = new Task(Listen); // Создание потока для получения сообщений
-                listeningTask.Start(); // Запуск потока
-                listeningTask.Wait(); // Не идем дальше пока поток не будет остановлен
+                listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // Создание сокета
+                Task listenTask = new Task(Listen); // Создание потока для получения сообщений
+                listenTask.Start(); // Запуск потока
+                listenTask.Wait(); // Не идем дальше пока поток не будет остановлен
             }
             catch (Exception ex)
             {
@@ -41,25 +42,25 @@ namespace Сhatterbox
             {
                 //Прослушиваем по адресу
                 IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), localPort);
-                listeningSocket.Bind(localIP);
+                listenSocket.Bind(localIP);
                 while (true)
                 {
                     StringBuilder builder = new StringBuilder(); // получаем сообщение
                     int bytes = 0; // количество полученных байтов
                     byte[] data = new byte[256]; // буфер для получаемых данных
-                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0); //адрес, с которого пришли данные
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0); //адрес, с которого пришли данныe
                     do
                     {
-                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        bytes = listenSocket.ReceiveFrom(data, ref remoteIp);
                         builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
                     }
-                    while (listeningSocket.Available > 0);
+                    while (listenSocket.Available > 0);
                     IPEndPoint remoteFullIp = remoteIp as IPEndPoint; // получаем данные о подключении
                     Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString()); // выводим сообщение
 
                     bool addClient = true; // Переменная для определения нового пользователя
                     for (int i = 0; i < clients.Count; i++) // Циклом перебераем всех пользователей которые отправляли сообщения на сервер
-                        if (clients[i].Address.ToString() == remoteFullIp.Address.ToString()) // Если аддресс отправителя данного сообщения совпадает с аддрессом в списке
+                        if (clients[i].Address.ToString() == remoteFullIp.Address.ToString()) // Если адрес отправителя данного сообщения совпадает с аддрессом в списке
                             addClient = false; // Не добавляем клиента в историю
                     if (addClient == true) // Если этого отправителя не было обноруженно в истории
                         clients.Add(remoteFullIp); // Добавляем клиента в исторю
@@ -83,17 +84,17 @@ namespace Сhatterbox
 
             for (int i = 0; i < clients.Count; i++) // Циклом перебераем всех клиентов
                 if (clients[i].Address.ToString() != ip) // Если аддресс получателя не совпадает с аддрессом отправителя
-                    listeningSocket.SendTo(data, clients[i]); // Отправляем сообщение
+                    listenSocket.SendTo(data, clients[i]); // Отправляем сообщение
         }
 
         // закрытие сокета
         private static void Close()
         {
-            if (listeningSocket != null)
+            if (listenSocket != null)
             {
-                listeningSocket.Shutdown(SocketShutdown.Both);
-                listeningSocket.Close();
-                listeningSocket = null;
+                listenSocket.Shutdown(SocketShutdown.Both);
+                listenSocket.Close();
+                listenSocket = null;
             }
             Console.WriteLine("Сервер остановлен!");
         }
--- СhatterboxClient/Program.cs
+++ СhatterboxClient/Program.cs
@@ -10,7 +10,7 @@ namespace ChatterboxClient
     {
         static int remotePort; // Порт для отправки сообщений
         static IPAddress ipAddress; // IP адрес сервера
-        static Socket listeningSocket; // Сокет
+        static Socket listenSocket; // Сокет
         static void Main(string[] args)
         {
             Console.WriteLine("UDP CHAT CLIENT");
@@ -22,16 +22,15 @@ namespace ChatterboxClient
             Console.WriteLine();
             try
             {
-                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // Создание сокета
+                listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); // Создание сокета
                 Task listeningTask = new Task(Listen); // Создание потока
                 listeningTask.Start(); // Запуск потока
-
                 while (true) // Отправление сообщений серверу в бесконечном цикле
                 {
                     string message = Console.ReadLine();
                     byte[] data = Encoding.Unicode.GetBytes(message);
                     EndPoint remotePoint = new IPEndPoint(ipAddress, remotePort);
-                    listeningSocket.SendTo(data, remotePoint);
+                    listenSocket.SendTo(data, remotePoint);
                 }
             }
             catch (Exception ex)
@@ -49,19 +48,20 @@ namespace ChatterboxClient
             try
             {
                 IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 0); // Прослушиваем по адресу
-                listeningSocket.Bind(localIP);
+                listenSocket.Bind(localIP);
                 while (true)
                 {
                     StringBuilder builder = new StringBuilder();
                     int bytes = 0;
                     byte[] data = new byte[256];
+                 
                     EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
                     do
                     {
-                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        bytes = listenSocket.ReceiveFrom(data, ref remoteIp);
                         builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
                     }
-                    while (listeningSocket.Available > 0);
+                    while (listenSocket.Available > 0);
                     IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
                     Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(), remoteFullIp.Port, builder.ToString());
                 }
@@ -78,11 +78,11 @@ namespace ChatterboxClient
         // закрытие сокета
         private static void Close()
         {
-            if (listeningSocket != null)
+            if (listenSocket != null)
             {
-                listeningSocket.Shutdown(SocketShutdown.Both);
-                listeningSocket.Close();
-                listeningSocket = null;
+                listenSocket.Shutdown(SocketShutdown.Both);
+                listenSocket.Close();
+                listenSocket = null;
             }
             Console.WriteLine("Сервер остановлен!");
         }
